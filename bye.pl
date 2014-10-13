65abe75d6fd6161d5e18370064613cc916a2bdb9
Amulya <amulyarnaik@gmail.com>
2014-10-13 15:59:24 +0530

    new 1

---
+++ bye.pl
@@ -0,0 +1,28 @@
+
+# XXX XXX XXX  THIS IS A NEW FILE XXX XXX XXX
+
+#!/usr/bin/perl -w
+
+#
+# bye.pl
+#
+# Developed by Sheeju Alex <sheeju@exceleron.com>
+# Copyright (c) 2014 Exceleron Inc
+# Licensed under terms of GNU General Public License.
+# All rights reserved.
+#
+# Changelog:
+# 2014-10-13 - created
+#
+
+# $Platon$
+
+use strict;
+
+$| = 1;
+
+print "n\ n";
+
+# vim: ts=4
+# vim600: fdm=marker fdl=0 fdc=3
+
