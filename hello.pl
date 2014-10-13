931e0c3d7a635c481177ae8ca8502feb4a0ec151
Amulya <amulyarnaik@gmail.com>
2014-10-13 16:50:19 +0530

    new4

--- hello.pl
+++ hello.pl
@@ -22,14 +22,13 @@ use Data::Dumper;
 
 $| = 1;
 
-<<<<<<< HEAD
 print "amulya\n";
-=======
 
 my $arr = [2,5,8,1,11,9];
 print Dumper $a;
->>>>>>> a4f7663eda0fb5cd78b6413d3fdf51217992e265
 print "hello world\n";
+
+my @qwert = (1,1,2);
 # vim: ts=4
 # vim600: fdm=marker fdl=0 fdc=3
 
