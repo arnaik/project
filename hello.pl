ec48349a57baec9b3046444a6b1e94a38ac5302a
Amulya <amulya@exceleron.com>
2014-10-13 04:48:03 -0500

    hello

---
+++ hello.pl
@@ -0,0 +1,28 @@
+
+# XXX XXX XXX  THIS IS A NEW FILE XXX XXX XXX
+
+#!/usr/bin/perl -w
+
+#
+# hello.pl
+#
+# Developed by Sheeju Alex <sheeju@exceleron.com>
+# Copyright (c) 2014 Exceleron Inc
+# Licensed under terms of GNU General Public License.
+# All rights reserved.
+#
+# Changelog:
+# 2014-10-13 - created
+#
+
+# $Platon$
+
+use strict;
+
+$| = 1;
+
+
+print "hello world\n";
+# vim: ts=4
+# vim600: fdm=marker fdl=0 fdc=3
+
